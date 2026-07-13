30a9b063b956aa18570f40132ffaf5b264984169
rgeorge70 <rgeorge70@gatech.edu>
2026-07-12 22:41:23 -0400

    basic project structure added

---
+++ src/main/java/com/tms/tm_auth_service/TmAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tms.tm_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tms/tm_auth_service/TmAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.tm_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
